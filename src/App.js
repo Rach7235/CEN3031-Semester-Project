f0903686dceb7a255124cede0214639f04763a27
Rach7235 <115181752+Rach7235@users.noreply.github.com>
2024-03-20 12:08:42 -0400

    Added tab navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import "react-native-gesture-handler";
 import {NavigationContainer} from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import JournalScreen from './screens/JournalScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import JournalScreen from './screens/JournalScreen';
 import DataScreen from './screens/DataScreen';
 
 
 
 
+
 const Stack =  createNativeStackNavigator()
+const Drawer =  createDrawerNavigator()
 
 export default function App() {
 
     return (  
         <NavigationContainer>
-            <Stack.Navigator initialRouteName={"Login"}>
-            <Stack.Screen name = "Login" component = {LoginScreen}/>
-            <Stack.Screen name = "Journal" component = {JournalScreen} />
-            <Stack.Screen name = "Data" component = {DataScreen}/>        
-            </Stack.Navigator>
+            <Drawer.Navigator>
+                <Drawer.Screen name = "Login" component = {LoginScreen}/>
+                <Drawer.Screen name = "Register" component = {RegisterScreen}/>
+                <Drawer.Screen name = "Journal" component = {JournalScreen}/>
+                <Drawer.Screen name = "Data" component = {DataScreen}/>
+            </Drawer.Navigator>
         </NavigationContainer>
     );
 }
