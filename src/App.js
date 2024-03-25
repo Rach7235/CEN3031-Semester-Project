09ca6c0520f6de50f32d5c012a407dc4d4982538
Rach7235 <115181752+Rach7235@users.noreply.github.com>
2024-03-25 18:58:19 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import "react-native-gesture-handler";
 import {NavigationContainer} from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import HomeScreen from './screens/HomeScreen';
 import JournalScreen from './screens/JournalScreen';
 import DataScreen from './screens/DataScreen';
 
+const StackNav=() => {
+    const Stack =  createNativeStackNavigator(); 
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name= "Login" component={LoginScreen}/>
+            <Stack.Screen name= "Register" component = {RegisterScreen}/>
+            <Stack.Screen name="Home" component={DrawerNav}/>
+            </Stack.Navigator>
+            
+    )
+}
 
-
-
-
-const Stack =  createNativeStackNavigator()
-const Drawer =  createDrawerNavigator()
-
-export default function App() {
+const DrawerNav=()=>{
+    const Drawer=createDrawerNavigator();
+    return (
+    <Drawer.Navigator>
+    <Drawer.Screen name= "Home" component={StackNav}/>
+    <Drawer.Screen name= "Journal" component={JournalScreen}/>
+    </Drawer.Navigator>
+    )
+}
+function App() {
 
     return (  
         <NavigationContainer>
-            <Drawer.Navigator>
-                <Drawer.Screen name = "Login" component = {LoginScreen}/>
-                <Drawer.Screen name = "Register" component = {RegisterScreen}/>
-                <Drawer.Screen name = "Journal" component = {JournalScreen}/>
-                <Drawer.Screen name = "Data" component = {DataScreen}/>
-            </Drawer.Navigator>
+        <DrawerNav/>
         </NavigationContainer>
     );
-}
+    
+}export default App;
