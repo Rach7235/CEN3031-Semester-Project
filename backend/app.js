2efd8b6c3eb5cf0c63c33bde9a2ca342b8948201
Rach7235 <115181752+Rach7235@users.noreply.github.com>
2024-03-25 18:56:53 -0400

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose=require("mongoose");
 app.use(express.json());
 
+
 const mongoUrl = "mongodb+srv://bquintero02:jA61o%5Ee3%25z@atlascluster.zxkgm5g.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
 mongoose.connect(mongoUrl).then(()=>{
     console.log("Database Connected");
@@ -18,14 +19,15 @@ const UserInfoSchema = new mongoose.Schema({
 },{
     collection:"userInfo"
 });
-const User=mongoose.model("UserInfo", UserInfoSchema);
+const User=mongoose.model("userInfo", UserInfoSchema);
 app.get("/", (req, res)=>{
     res.send({status:"Started"})
 });
 
+// API name is register
 app.post('/register', async(req,res)=>{
-    const {name, password}=req.body;
-    const oldUser = await User.findOne({name:name});
+    const {id, password}= req.body;
+    const oldUser = await User.findOne({_id:id});
 
 // If user's ID is already registered, the user already exists
 if (oldUser) {
@@ -34,12 +36,27 @@ if (oldUser) {
     try {
         // Otherwise, create user in userInfo collection with schema
         await User.create({
-            _id:name,
+            _id:id,
             pass:password,
         });
         res.send({status:"ok",data:"User Created"});
     } catch(error) {
         res.send({status:"error", data: error});
+    }
+})
+
+// API name is login
+app.post('/login', async(req, res) => {
+    const {id, password} = req.body;
+    const oldUser= await User.findOne({_id:id})
+
+    // If user tries to login to an unregistered ID, user doesn't exist
+    if (!oldUser) {
+        return res.send({data:"User doesn't exist"})
+    }
+
+    if (res.status(201)) {
+        return res.send({status:"ok", data:"Logged in"})
     }
 })
 
