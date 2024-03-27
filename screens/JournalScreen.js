25cef0e668ee130253095a60f4c019c9c37c4e4a
Rach7235 <115181752+Rach7235@users.noreply.github.com>
2024-03-27 17:01:04 -0400

    Update JournalScreen.js

--- screens/JournalScreen.js
+++ screens/JournalScreen.js
@@ -1,13 +1,39 @@
-import { StyleSheet, Text, View, TextInput, SafeAreaView, Button} from 'react-native';
-import React, { useMemo, useState } from 'react';
+mport { StyleSheet, Text, View, TextInput, SafeAreaView, Button, Alert} from 'react-native';
+import React, { useMemo, useState, useEffect} from 'react';
 import RadioButton from 'react-native-radio-buttons-group';
+import axios from 'axios';
+
+
 
 
 const JournalScreen = ({navigation}) => {
+  const [entry, setEntry] = useState('');
+  var dateVariable = Date().toLocaleString();
+
+  function handleEntry(e) {
+    const entryVar=e.nativeEvent.text;
+    setEntry(entryVar);
+  }
+  function handleSave() {
+    const journalData = {
+      id: 'test',
+      date: dateVariable,
+      entry: entry,
+    }
+      axios.post("http://192.168.254.28:8001/journal", journalData).
+      then(res => {
+        console.log(res.data);
+        if (res.data.status =="ok") {
+           // Journal entry successfully created
+          Alert.alert("Journal Entry Created!");
+        }
+      })
+      .catch(e => console.log(e));
+      }
     const radioButtons = useMemo(() => ([
         {
             id: '1', 
-            label: 'Very sad',
+            label: 'Very Sad',
         },
         {
             id: '2',
@@ -30,22 +56,25 @@ const JournalScreen = ({navigation}) => {
     
 
 
-    const [selectedId, setSelectedId] = useState();
-
+  
   return (
     <SafeAreaView style = {styles.container}>
       <TextInput
           style = {styles.input}
+          onChange={e => handleEntry(e)}
         />
         <View style = {styles.space}
         />
         <Text>How are you feeling today?</Text>
          <RadioButton
             radioButtons={radioButtons} 
-            onPress={setSelectedId} 
-            selectedId={selectedId}
+            //onPress={setSelectedId} 
+            //selectedId={selectedId}
         />
-        
+        <Button
+          title = 'Save'
+          onPress={() => handleSave()}
+        />
     </SafeAreaView>
   )
 }
@@ -68,12 +97,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fff',
   },
-
-  input: {
+    input: {
+    textAlignVertical:'top',
     borderWidth: 2,
     borderColor: 'black',
     width: 340,
     height:340
+    
   },
   
 });
