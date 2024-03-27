a3634cd14541b208e48978f744c13c4912e3ee84
Rach7235 <115181752+Rach7235@users.noreply.github.com>
2024-03-27 16:49:50 -0400

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,47 @@
-import { StyleSheet, Text, View, TextInput, SafeAreaView, Button, ScrollView} from 'react-native';
-import React, { Component } from 'react';
+import {Alert, StyleSheet, Text, View, TextInput, SafeAreaView, Button, ScrollView} from 'react-native';
+import React, { Component, useEffect, useState} from 'react';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const HomeScreen = () => {
+const HomeScreen = (props) => { const {navigation} = props; 
+    const [userData, setUserData] = useState('');
+    async function getData() {
+        const token = AsyncStorage.getItem('token');
+        console.log(token);
+        axios.post("http://192.168.254.28:8001/userdata", {token:token})
+        .then(res=> {
+            console.log(res.data);
+            setUserData(res.data.data);
+        })
+    }
+       
+    
+    
+    useEffect(() => {
+        getData();
+    }, []);
+    
+function signOut() {
+    AsyncStorage.setItem('isLoggedIn','');
+    AsyncStorage.setItem('token', '');
+  // Need to change this to keep user logged in
+    navigation.navigate("Login");
+    Alert.alert("Sign out successful!")
+}
 
   return (
+    <SafeAreaView style = {styles.container}>
     <Text>Welcome!</Text>
+    <View style={styles.space}/>
+    <Button 
+    title = 'Sign Out'
+    onPress={() => signOut()}
+    />
+    </SafeAreaView>
   )
+
 }
+
 
 export default HomeScreen
 
